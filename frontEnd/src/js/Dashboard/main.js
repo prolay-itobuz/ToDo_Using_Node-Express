3a0f4d28d6ec9c871c09e36c81ff9de83503b836
prolay-itobuz <prolay@itobuz.com>
2025-10-21 17:51:25 +0530

    Delete frontEnd directory

--- frontEnd/src/js/Dashboard/main.js
+++
@@ -1,5 +0,0 @@
-// Import our custom CSS
-import "../../scss/styles.scss";
-import "../Dashboard/operations/app.js";
-import "../Dashboard/operations/updateTasks.js";
-import "../Dashboard/operations/searchTasks.js";
